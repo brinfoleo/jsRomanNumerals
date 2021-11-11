21f77fc3d4e04d4ec3c1fd53219d6bf2e2504d72
Leonardo A <brinfo.leo@gmail.com>
2021-11-11 12:52:38 +0000

    Implementing Roman to Numeral Conversion. Logic learned from my friend Jasbier Dhaliwal in the repository https://github.com/bikerJas/roman_numerals_exercise

--- jsRomanNumerals.js
+++ jsRomanNumerals.js
@@ -1,11 +1,34 @@
 function toRoman(num) {
-    const numRoman=[['','I','II','III','IV','V','VI','VII','VIII','IX'],
-                    ['','X','XX','XXX','XL','L','LX','LXX','LXXX','XC']]
-    let n=[...num.toString()];
-    let newRoman='';
-    for (i=0;i<n.length;i++){
-        newRoman= numRoman[i][n[n.length-(i+1)]] + newRoman;
+    const numRoman = [['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
+    ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'],
+    ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']]
+    let n = [...num.toString()];
+    let newRoman = '';
+    for (i = 0; i < n.length; i++) {
+        newRoman = numRoman[i][n[n.length - (i + 1)]] + newRoman;
     }
     return newRoman;
-  }
-  module.exports = toRoman;
+}
+
+function toNumeral(nRoman) {
+    if (nRoman.match(/^(?=[MDCLXVI])M{0,3}(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$/i) === null) return null;
+
+    const romanNumeral = {
+        I: 1, IV: 4, V: 5, IX: 9,
+        X: 10, XL: 40, L: 50, XC: 90,
+        C: 100, CD: 400, D: 500, CM: 900,
+        M: 1000
+    }
+    const arrayRomanNumeral = [...nRoman];
+    let sumNumber = 0;
+    for (let i = 0; i < arrayRomanNumeral.length; i++) {
+        let tmpRomanNumeral = arrayRomanNumeral[i] + arrayRomanNumeral[++i];
+        if (romanNumeral[tmpRomanNumeral] == undefined) {
+            tmpRomanNumeral = arrayRomanNumeral[--i];
+        }
+        sumNumber += romanNumeral[tmpRomanNumeral];
+    }
+    return sumNumber;
+}
+
+module.exports = { toRoman, toNumeral };
--- jsRomanNumerals.test.js
+++ jsRomanNumerals.test.js
@@ -1,4 +1,5 @@
-const toRoman = require('./jsRomanNumerals');
+const toRoman = require('./jsRomanNumerals').toRoman;
+const toNumeral = require('./jsRomanNumerals').toNumeral;
 
 test('convert number 1 to Roman Numeral I', () => {
     expect(toRoman(1)).toBe('I');
@@ -14,4 +15,67 @@ test('convert number 10 to Roman Numeral X', () => {
 });
 test('convert number 29 to Roman Numeral XXIX', () => {
     expect(toRoman(29)).toBe('XXIX');
+});
+test('convert number 129 to Roman Numeral CXXIX', () => {
+    expect(toRoman(129)).toBe('CXXIX');
+});
+
+test('convert number 500 to Roman Numeral D', () => {
+    expect(toRoman(500)).toBe('D');
+});
+test('convert number 100 to Roman Numeral C', () => {
+    expect(toRoman(100)).toBe('C');
+});
+
+//Convert Roman to Numeral
+
+test("checks that I returns 1", () => {
+    expect(toNumeral("I")).toBe(1);
+});
+
+test("checks that II returns 2", () => {
+    expect(toNumeral("II")).toBe(2);
+});
+
+test("checks that IV returns 4", () => {
+    expect(toNumeral("IV")).toBe(4);
+});
+
+test("checks that IX returns 9", () => {
+    expect(toNumeral("IX")).toBe(9);
+});
+
+test("checks that IIII returns null", () => {
+    expect(toNumeral("IIII")).toBe(null);
+});
+
+test("checks that XX returns 20", () => {
+    expect(toNumeral("XX")).toBe(20);
+});
+
+test("checks that XL returns 40", () => {
+    expect(toNumeral("XL")).toBe(40);
+});
+
+test("checks that LL returns null", () => {
+    expect(toNumeral("LL")).toBe(null);
+});
+
+test("checks that CD returns 400", () => {
+    expect(toNumeral("CD")).toBe(400);
+});
+
+test("checks that DD returns null", () => {
+    expect(toNumeral("DD")).toBe(null);
+});
+
+test("checks that MM returns 2000", () => {
+    expect(toNumeral("MM")).toBe(2000);
+});
+
+test("checks that MCMLXXXVII returns 1987", () => {
+    expect(toNumeral("MCMLXXXVII")).toBe(1987);
+});
+test("checks that CMXLIX returns CMXLIX", () => {
+    expect(toNumeral("CMXLIX")).toBe(949);
 });
